fddf786af4aed06f4d7b700eb4540c4ab228c831
Kishwar <kishwar.shafin@gmail.com>
2017-10-15 20:11:43 -0700

    initial commit to diff

--- src/modules/model.py
+++ src/modules/model.py
@@ -1,15 +1,18 @@
 import torch.nn as nn
 import torch.nn.functional as F
-
+from torch.autograd import Variable
 
 # CNN Model (2 conv layer)
 class CNN(nn.Module):
-    def __init__(self, inChannel=1, outChannel=256, coverageDepth=34, classN=3, window_size=1):
+    def __init__(self, inChannel=1, outChannel=256, coverageDepth=34, hidden_number=200, hidden_layer=3, classN=3, bidirection=False):
         super(CNN, self).__init__()
         self.inChannel = inChannel
         self.outChannel = outChannel
         self.coverageDepth = coverageDepth
         self.classN = classN
+        self.hidden_number = hidden_number
+        self.hidden_layer = hidden_layer
+        self.direction = 1 if not bidirection else 2
         # -----CNN----- #
         self.batchNorm = nn.BatchNorm2d(outChannel)
         self.incpConv0 = nn.Conv2d(inChannel, outChannel, (1, 1), bias=False, stride=(1, 1))
@@ -19,10 +22,12 @@ class CNN(nn.Module):
         self.conv2 = nn.Conv2d(outChannel, outChannel, (1, 3), padding=(0, self.coverageDepth * 3), bias=False,
                                stride=(1, 3))
         self.conv3 = nn.Conv2d(outChannel, outChannel, (1, 1), bias=False)
+        # -----RNN----- #
+        self.lstm1 = nn.LSTM(coverageDepth * 3, hidden_number, hidden_layer, bidirectional=bidirection)
         # -----FCL----- #
-        self.fc1 = nn.Linear(outChannel * coverageDepth * 3 * window_size, 100)
-        self.fc2 = nn.Linear(100, 30)
-        self.fc3 = nn.Linear(30, self.classN)
+        self.fc1 = nn.Linear(self.hidden_number * self.direction, self.hidden_number)
+        self.fc2 = nn.Linear(self.hidden_number, self.classN * 10)
+        self.fc3 = nn.Linear(self.classN * 10, self.classN)
         self.fc4 = nn.LogSoftmax()
 
     def residual_layer(self, input_data, layer, batch_norm_flag=False):
@@ -37,12 +42,34 @@ class CNN(nn.Module):
         x = F.relu(indataCp + convOut3)
         return x
 
+    def repackage_hidden(self, h):
+        """Wraps hidden states in new Variables, to detach them from their history."""
+        if type(h) == Variable:
+            return Variable(h.data)
+        else:
+            return tuple(self.repackage_hidden(v) for v in h)
+
+    def init_hidden(self, seq_len):
+        weight = next(self.parameters()).data
+        return (Variable(weight.new(self.direction * self.hidden_layer, seq_len, self.hidden_number).zero_()),
+                Variable(weight.new(self.direction * self.hidden_layer, seq_len, self.hidden_number).zero_()))
+
+    def lstm_layer(self, input):
+        hidden = self.init_hidden(input.size(2))
+        for i in range(input.size(0)):
+            out, hidden = self.lstm1(input[i], hidden)
+            hidden = self.repackage_hidden(hidden)
+        return out
+
     def fully_connected_layer(self, x):
-        batch_size = x.size(0)
-        x = x.view([batch_size, -1])
+        batch_size = x.size(1)
+        seq_length = x.size(0)
+        x = x.view([batch_size * seq_length, -1])
+
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
+        x = x.view([batch_size, seq_length, self.classN])
         return x
 
     def forward(self, x):
@@ -52,6 +79,9 @@ class CNN(nn.Module):
         x = self.residual_layer(x, layer=3)
         x = self.residual_layer(x, layer=4)
 
+        x = x.permute(1, 2, 0, 3)
+
+        x = self.lstm_layer(x)
         x = self.fully_connected_layer(x)
         x = self.fc4(x)
         return x.view(-1, 3)
