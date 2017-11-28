0e0d7bb7256a11149985c82387298a9d8f8d236d
Kishwar <kishwar.shafin@gmail.com>
2017-11-27 22:53:56 -0800

    model loading and saving module changed.
    Plotting changed for better visualization and use

--- src/utils/plots/plot.py
+++ src/utils/plots/plot.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+import seaborn as sns
+sns.set(color_codes=True)
 
 file_name = sys.argv[1]
 val_count = 0
@@ -8,21 +10,23 @@ valx = []
 valy = []
 testx = []
 testy = []
+batch_size = 469
+epoch = 2
 with open(file_name, "r") as ins:
     for line in ins:
         line = line.rstrip()
         if not line:
             continue
-        if line[0] == 'V':
+        if line[0] == 'T':
             val_count += 1
-            val_x = int(val_count) * 10
-            val_y = float(line.split(' ')[4])
+            val_x = int(val_count) * batch_size
+            val_y = float(line.split(' ')[2])
             valx.append(val_x)
             valy.append(val_y)
-            print(val_x, val_y)
+            #print(val_x, val_y)
         else:
             split_list = line.split('\t')
-            x = int(split_list[0]) * 10 + int(split_list[1])
+            x = int(split_list[0]) * batch_size + int(split_list[1])
             y = float(split_list[2])
             testx.append(x)
             testy.append(y)
@@ -30,8 +34,16 @@ with open(file_name, "r") as ins:
 test, = plt.plot(testx, testy)
 val, = plt.plot(valx, valy, 'ro-')
 plt.legend([test, val], ['Train loss', 'Hold out loss'])
-plt.xticks(range(0, 600, 100), ('0', '10', '20', '30', '40', '50'))
+
+gap_in_ticks = 1
+x_ticks = ()
+for i in range(0, epoch+1, gap_in_ticks):
+    x_ticks = x_ticks + (str(i),)
+plt.ylim([0, 0.000002])
+print(x_ticks)
+plt.xticks(range(0, batch_size * (epoch+1), batch_size*gap_in_ticks), x_ticks)
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
-plt.title('With Adam optimizer')
-plt.savefig('Loss-Adam.png')
+
+# plt.title('CNN Training chr1~19')
+plt.savefig(file_name+'-viz'+'.png', dpi=400)
